6ac186a2c7f4b15c0709e3fa6c6b53815c80d1bd
zzzzz1532 <user@192.168.0.15>
2023-09-11 02:14:56 +0800

    0910 刊版頁 AJAX

---
+++ src/main/java/com/ispan/eeit69/model/PendingOrder.java
@@ -0,0 +1,121 @@
+package com.ispan.eeit69.model;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="PendingOrder")
+public class PendingOrder implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	// Model 版本號 => 1
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer orderId; //編號
+	private Integer orderNo; // 訂單號
+	private String diningLocation; //內用外帶
+	private String foodName; // 品名
+	private Integer foodQuantity; // 數量
+	private Integer orderPrice; //訂單總額
+	private Timestamp created_at; // 生成時間
+	
+	
+	public PendingOrder()  {
+		
+	}
+
+
+	public PendingOrder(Integer orderId, Integer orderNo, String diningLocation, String foodName, Integer foodQuantity,
+			Integer orderPrice, Timestamp created_at) {
+		super();
+		this.orderId = orderId;
+		this.orderNo = orderNo;
+		this.diningLocation = diningLocation;
+		this.foodName = foodName;
+		this.foodQuantity = foodQuantity;
+		this.orderPrice = orderPrice;
+		this.created_at = created_at;
+	}
+
+
+	public Integer getOrderId() {
+		return orderId;
+	}
+
+
+	public void setOrderId(Integer orderId) {
+		this.orderId = orderId;
+	}
+
+
+	public Integer getOrderNo() {
+		return orderNo;
+	}
+
+
+	public void setOrderNo(Integer orderNo) {
+		this.orderNo = orderNo;
+	}
+
+
+	public String getDiningLocation() {
+		return diningLocation;
+	}
+
+
+	public void setDiningLocation(String diningLocation) {
+		this.diningLocation = diningLocation;
+	}
+
+
+	public String getFoodName() {
+		return foodName;
+	}
+
+
+	public void setFoodName(String foodName) {
+		this.foodName = foodName;
+	}
+
+
+	public Integer getFoodQuantity() {
+		return foodQuantity;
+	}
+
+
+	public void setFoodQuantity(Integer foodQuantity) {
+		this.foodQuantity = foodQuantity;
+	}
+
+
+	public Integer getOrderPrice() {
+		return orderPrice;
+	}
+
+
+	public void setOrderPrice(Integer orderPrice) {
+		this.orderPrice = orderPrice;
+	}
+
+
+	public Timestamp getCreated_at() {
+		return created_at;
+	}
+
+
+	public void setCreated_at(Timestamp created_at) {
+		this.created_at = created_at;
+	}
+
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+}
