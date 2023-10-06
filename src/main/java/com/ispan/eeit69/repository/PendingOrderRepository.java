2026e10ef71d41227642ea2a769c3ee33920fca1
h61685 <136684927+h61685@users.noreply.github.com>
2023-10-06 17:26:24 +0800

    Update PendingOrderRepository.java

--- src/main/java/com/ispan/eeit69/repository/PendingOrderRepository.java
+++ src/main/java/com/ispan/eeit69/repository/PendingOrderRepository.java
@@ -1,7 +1,8 @@
 package com.ispan.eeit69.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -10,24 +11,17 @@ import com.ispan.eeit69.model.PendingOrder;
 
 public interface PendingOrderRepository extends JpaRepository<PendingOrder, Integer> {
     
-	
-@Modifying
- @Query("SELECT new com.ispan.eeit69.model.PendingOrderSummary(" +
-          "o.orderNo, o.diningLocation, " 
-		  + "GROUP_CONCAT(o.categoryName), "
-		  + "GROUP_CONCAT(o.productName),"
-		  + "GROUP_CONCAT(o.labelName)," 
-		  + "GROUP_CONCAT(o.foodNote)," 
-          + "GROUP_CONCAT(o.orderNote),"
-          + "GROUP_CONCAT(o.foodQuantity), SUM(o.orderPrice), " 
-          + "o.created_at) " 
-          + "FROM PendingOrder o " 
-          + "WHERE o.orderNo = :orderNo " 
-          + "GROUP BY o.orderNo, o.diningLocation, o.created_at")
-  List<PendingOrderSummary> findOrderSummaryByOrderNo(@Param("orderNo") Integer orderNo);
-    
-    @Modifying
-    @Query("DELETE FROM PendingOrder p WHERE p.orderNo = :orderNo")
-    void deleteById(@Param("orderNo") Integer orderNo);
-}
+	@Query(value = "SELECT " +
+	        "  po.orderNo as orderNo, " +
+	        "  po.diningLocation as diningLocation, " +
+	        "  MIN(po.created_at) as createdAt, " +
+	        "  SUM(CAST(po.orderPrice AS DECIMAL(10, 2))) as totalOrderPrice " +
+	        "FROM PendingOrder po " +
+	        "GROUP BY po.orderNo, po.diningLocation,po.diningLocation", nativeQuery = true)
+	List<Object[]> findOrderDetailsForAllOrders();
 
+    
+    @Query("DELETE FROM PendingOrder o WHERE o.orderNo = :orderNo")
+    void deleteById(@Param("orderNo") Integer orderNo);
+
+}
