66128481ff71d042cf98fcdd89c2835c60f4deb8
zzzzz1532 <zzzzz1532@gmail.com>
2023-10-06 09:54:27 +0800

    add willys index

--- src/main/java/com/ispan/eeit69/controller/FindPendingOrderController.java
+++ src/main/java/com/ispan/eeit69/controller/FindPendingOrderController.java
@@ -1,13 +1,9 @@
 package com.ispan.eeit69.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.ispan.eeit69.model.PendingOrder;
 import com.ispan.eeit69.service.PendingOrderService;
 
 @Controller
